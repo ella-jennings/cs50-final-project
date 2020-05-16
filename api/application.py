b4fa4fc622cd792e7cf515d173c6aac8fe986ba3
Ella Fahie <ella.fahie@thoughtworks.com>
2020-05-16 14:33:09 -0700

    Added react frontent

--- api/application.py
+++ api/application.py
@@ -6,4 +6,6 @@ app = Flask(__name__, template_folder='../html')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", token="Hello react")
+
+app.run(debug=True)
