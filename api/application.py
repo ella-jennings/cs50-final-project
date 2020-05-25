0b61647e2804bfe06249dcc39a85e4cfd4cdf27b
Ella Fahie <ella.fahie@thoughtworks.com>
2020-05-24 17:41:42 -0700

    remove redundant flask app

--- api/application.py
+++
@@ -1,9 +0,0 @@
-import flask
-
-app = flask.Flask(__name__, template_folder="static/templates")
-
-@app.route('/')
-def index():
-    return flask.render_template("index.html", token="Hello react")
-
-app.run(debug=True)
