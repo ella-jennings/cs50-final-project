632a4b8ec3474ffd741243660b762ba97ffeb125
Ella Fahie <ella.fahie@thoughtworks.com>
2020-05-24 18:58:20 -0700

    card selected based off city state

--- final-project/src/components/card/card.jsx
+++ final-project/src/components/card/card.jsx
@@ -1,16 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Deal} from '../../constants';
 import DealIcon from '../deal/deal';
 import './card.scss'
 
+
 const Card = (props) => {
-    const [selected, setSelected] = useState(false);
-    let isSelected = selected === true ? "selected" : "unselected";
+    console.log(props);
+    let className = props.selected === true ? "selected" : "unselected";
+    const changeSelected = () => {
+        console.log('got here');
+        props.setAsSelected(props.reference);
+    }
     
     const value = props.value === Deal ? <DealIcon/> : props.value;
     console.log(props)
     return(
-        <button name={props.value} className={`card card_${isSelected} card_${props.colour}`} onClick={() => setSelected(!selected)}>{value}</button>
+        <button name={props.value} className={`card card_${className} card_${props.colour}`} onClick={() => changeSelected()}>{value}</button>
     )
 }
 
--- final-project/src/components/city/city.jsx
+++ final-project/src/components/city/city.jsx
@@ -3,24 +3,100 @@ import Card from '../card/card.jsx'
 import './city.css';
 import {Deal} from '../../constants'
 
+const InitialCards = {
+    Deal1: {
+        selected: false,
+        value: Deal,
+        reference: "Deal1",
+    },
+    Deal2: {
+        selected: false,
+        value: Deal,
+        reference: "Deal2",
+    },
+    Deal3: {
+        selected: false,
+        value: Deal,
+        reference: "Deal3",
+    },
+    2: {
+        selected: false,
+        value: 2,
+        reference: 2,
+    },
+    3: {
+        selected: false,
+        value: 3,
+        reference: 3,
+    },
+    4: {
+        selected: false,
+        value: 4,
+        reference: 4,
+    },
+    5: {
+        selected: false,
+        value: 5,
+        reference: 5,
+    },
+    6: {
+        selected: false,
+        value: 6,
+        reference: 6,
+    },
+    7: {
+        selected: false,
+        value: 7,
+        reference: 7,
+    },
+    8: {
+        selected: false,
+        value: 8,
+        reference: 8,
+    },
+    9: {
+        selected: false,
+        value: 9,
+        reference: 9,
+    },
+    10: {
+        selected: false,
+        value: 10,
+        reference: 10,
+    },
+}
+
+
 
 const City = (props) => {
+    const [cards, updateCards] = useState(InitialCards);
+
+    const setSelected = (value) => {
+        let newCards = {...cards};
+        newCards[value].selected = !newCards[value].selected;
+        updateCards(newCards);
+        console.log(cards.Deal1.selected)
+    }
     const [total, calculateTotal] = useState(0);
+    const genericProps = {
+        setAsSelected: (value) => setSelected(value),
+        colour: props.colour,
+    }
     const className = `city_${props.colour}`;
     return (
         <div className={`${className} city`}>
-            <Card colour={props.colour} value={Deal}/>
-            <Card colour={props.colour} value={Deal}/>
-            <Card colour={props.colour} value={Deal}/>
-            <Card colour={props.colour} value={2}/>
-            <Card colour={props.colour} value={3}/>
-            <Card colour={props.colour} value={4}/>
-            <Card colour={props.colour} value={5}/>
-            <Card colour={props.colour} value={6}/>
-            <Card colour={props.colour} value={7}/>
-            <Card colour={props.colour} value={8}/>
-            <Card colour={props.colour} value={9}/>
-            <Card colour={props.colour} value={10}/>
+            <Card {...genericProps} {...cards.Deal1}/>
+            <Card {...genericProps} {...cards.Deal2}/>
+            <Card {...genericProps} {...cards.Deal3}/>
+            <Card {...genericProps} {...cards[2]}/>
+            <Card {...genericProps} {...cards[3]}/>
+            <Card {...genericProps} {...cards[4]}/>
+            <Card {...genericProps} {...cards[5]}/>
+            <Card {...genericProps} {...cards[6]}/>
+            <Card {...genericProps} {...cards[7]}/>
+            <Card {...genericProps} {...cards[8]}/>
+            <Card {...genericProps} {...cards[9]}/>
+            <Card {...genericProps} {...cards[10]}/>
             <p>Total: {total}</p>
         </div>
     )
