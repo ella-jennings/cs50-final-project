1621ce295038b432cecd65fc48f13416c7134846
Ella Fahie <ella.fahie@thoughtworks.com>
2020-05-25 12:31:26 -0700

    Updates

---
+++ precache-manifest.6c74a2da187ae38eecf67be9520d4758.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a68f457541577935f6f7cd4974a72a",
+    "url": "/lost-cities-calculator/index.html"
+  },
+  {
+    "revision": "d88ce97ec157d2e158c7",
+    "url": "/lost-cities-calculator/static/css/main.48ffcf98.chunk.css"
+  },
+  {
+    "revision": "0f073b4995065f479244",
+    "url": "/lost-cities-calculator/static/js/2.84804f03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lost-cities-calculator/static/js/2.84804f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88ce97ec157d2e158c7",
+    "url": "/lost-cities-calculator/static/js/main.f99e5761.chunk.js"
+  },
+  {
+    "revision": "f5ef965bdd400ed6c22b",
+    "url": "/lost-cities-calculator/static/js/runtime-main.eb7984d1.js"
+  },
+  {
+    "revision": "00f32eed4bf8d4c74204018b7047edd1",
+    "url": "/lost-cities-calculator/static/media/blue_background.00f32eed.jpg"
+  },
+  {
+    "revision": "c510b77193f292e896107c67884de491",
+    "url": "/lost-cities-calculator/static/media/green_background.c510b771.jpg"
+  },
+  {
+    "revision": "25ae9fb4f1050caf7b3d5378c5e976e4",
+    "url": "/lost-cities-calculator/static/media/purple_background.25ae9fb4.jpg"
+  },
+  {
+    "revision": "4b2977c18a8fd0a6b9f0ea2cf3ed74b4",
+    "url": "/lost-cities-calculator/static/media/red_background.4b2977c1.jpg"
+  },
+  {
+    "revision": "9bbc89abf57a31ef37180acc8d56a7d7",
+    "url": "/lost-cities-calculator/static/media/white_background.9bbc89ab.jpg"
+  },
+  {
+    "revision": "15a8b7acd0612327fc5a6c31a88d6b8c",
+    "url": "/lost-cities-calculator/static/media/yellow_background.15a8b7ac.jpg"
+  }
+]);
