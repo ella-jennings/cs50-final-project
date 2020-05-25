ddc8814202d4b4beb014dc08a74f9e2c48d0a03a
Ella Fahie <ella.fahie@thoughtworks.com>
2020-05-25 12:07:20 -0700

    Added build folder for github pages

---
+++ final-project/build/precache-manifest.a9e471a2d34d0e67ded6e447a6b6d94b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770b5405154ae0f1df23c83dde87fcfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e95260733a808f86fe3c",
+    "url": "/static/css/main.f9054a26.chunk.css"
+  },
+  {
+    "revision": "0f073b4995065f479244",
+    "url": "/static/js/2.84804f03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84804f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95260733a808f86fe3c",
+    "url": "/static/js/main.ba3d1668.chunk.js"
+  },
+  {
+    "revision": "e7b92fe08f118a7b0777",
+    "url": "/static/js/runtime-main.375f7f6f.js"
+  },
+  {
+    "revision": "00f32eed4bf8d4c74204018b7047edd1",
+    "url": "/static/media/blue_background.00f32eed.jpg"
+  },
+  {
+    "revision": "c510b77193f292e896107c67884de491",
+    "url": "/static/media/green_background.c510b771.jpg"
+  },
+  {
+    "revision": "25ae9fb4f1050caf7b3d5378c5e976e4",
+    "url": "/static/media/purple_background.25ae9fb4.jpg"
+  },
+  {
+    "revision": "4b2977c18a8fd0a6b9f0ea2cf3ed74b4",
+    "url": "/static/media/red_background.4b2977c1.jpg"
+  },
+  {
+    "revision": "9bbc89abf57a31ef37180acc8d56a7d7",
+    "url": "/static/media/white_background.9bbc89ab.jpg"
+  },
+  {
+    "revision": "15a8b7acd0612327fc5a6c31a88d6b8c",
+    "url": "/static/media/yellow_background.15a8b7ac.jpg"
+  }
+]);
