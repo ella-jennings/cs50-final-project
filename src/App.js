d7887fd3cd5b10802c1e2c411a97005f5f1a02db
Ella Fahie <ella.fahie@thoughtworks.com>
2020-06-06 20:43:33 -0700

    settings button added to homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Cities from './components/cities/cities';
+import SettingsButton from './components/settingsButton/settingsButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Lost Cities Calculator</h1>
+        <h1 className="title">Lost Cities Calculator</h1>
+        <div className="settingsContainer">
+          <SettingsButton openModal={() => console.log("clicked")}/>
+        </div>
       </header>
       <Cities fiveCities={false}/>
       <p>svg obtained from <a href="https://fontawesome.com/license">FontAwesome</a></p>
